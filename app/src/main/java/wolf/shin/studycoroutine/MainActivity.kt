687df6bb15dbb8f90298f03dabb64b5ee2d7001e
shindonghwi <ehdgnl8940@naver.com>
2022-05-01 01:09:42 +0900

    Unconfined를 사용할때, 중단점이 있다면 수행되는 디스패처가 변경된다. Unconfined보다는 확실한 디스패처를 사용하자

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -17,22 +17,13 @@ class MainActivity : ComponentActivity() {
             StudyCoroutineTheme {
 
                 runBlocking {
-
-                    // main
-                    launch { Log.d(TAG, "1 부모 컨텍스트: ${Thread.currentThread().name}") }
-
-                    // DefaultDispatcher- Core 수에 비례하는 스레프 풀에서 수행됨.
-                    launch(Dispatchers.Default) { Log.d(TAG, "2 Default ${Thread.currentThread().name}") }
-
-                    // DefaultDispatcher- Core 수 보다 훨씬 많은 스레드를 가지는 스레드 풀, IO 작업은 CPU를 덜 사용한다.
-                    launch(Dispatchers.IO) { Log.d(TAG, "4 IO ${Thread.currentThread().name}") }
-
-                    // main 어디에도 속하지 않음. 어느 스레드에서 수행되는지 정확히 알 수 없다.
-                    launch(Dispatchers.Unconfined) { Log.d(TAG, "5 Unconfined ${Thread.currentThread().name}") }
-
-                    // new Thread 새로운 스레드를 생성하여 동작함.
-                    launch(newSingleThreadContext("Wolf Developer")) { Log.d(TAG, "6 newSingleThreadContext ${Thread.currentThread().name}") }
-
+                    async(Dispatchers.Unconfined){
+                        Log.d(TAG, "${Thread.currentThread().name}")
+                        delay(100L)
+                        Log.d(TAG, "${Thread.currentThread().name}")
+                        delay(100L)
+                        Log.d(TAG, "${Thread.currentThread().name}")
+                    }
                 }
 
             }
