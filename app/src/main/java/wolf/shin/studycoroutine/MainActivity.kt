d677c4635cf3c645d960acd281e87f022f74a63b
shindonghwi <ehdgnl8940@naver.com>
2022-05-01 01:04:57 +0900

    코루틴 디스패처의 종류 - Default, IO, Unconfined, newSingleThreadContext 사용하기

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
 import kotlin.random.Random
 
@@ -18,40 +15,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudyCoroutineTheme {
+
                 runBlocking {
-                    try {
-                        doSomething()
-                    } finally {
-                        Log.d(TAG, "doSomething failed")
-                    }
+
+                    // main
+                    launch { Log.d(TAG, "1 부모 컨텍스트: ${Thread.currentThread().name}") }
+
+                    // DefaultDispatcher- Core 수에 비례하는 스레프 풀에서 수행됨.
+                    launch(Dispatchers.Default) { Log.d(TAG, "2 Default ${Thread.currentThread().name}") }
+
+                    // DefaultDispatcher- Core 수 보다 훨씬 많은 스레드를 가지는 스레드 풀, IO 작업은 CPU를 덜 사용한다.
+                    launch(Dispatchers.IO) { Log.d(TAG, "4 IO ${Thread.currentThread().name}") }
+
+                    // main 어디에도 속하지 않음. 어느 스레드에서 수행되는지 정확히 알 수 없다.
+                    launch(Dispatchers.Unconfined) { Log.d(TAG, "5 Unconfined ${Thread.currentThread().name}") }
+
+                    // new Thread 새로운 스레드를 생성하여 동작함.
+                    launch(newSingleThreadContext("Wolf Developer")) { Log.d(TAG, "6 newSingleThreadContext ${Thread.currentThread().name}") }
+
                 }
+
             }
         }
-    }
-}
-
-suspend fun getRandom1(): Int {
-    try {
-        delay(1000L)
-        return Random.nextInt(0, 500)
-    } finally {
-        Log.d(TAG, "getRandom1 cancel")
-    }
-
-}
-
-suspend fun getRandom2(): Int {
-    delay(1000L)
-    throw IllegalStateException()
-}
-
-suspend fun doSomething() = coroutineScope {
-    val value1 = async { getRandom1() }
-    val value2 = async { getRandom2() }
-
-    try {
-        Log.d(TAG, "result ${value1.await()} + ${value2.await()} = ${value1.await() + value2.await()}")
-    } finally {
-        Log.d(TAG, "doSomething cancel")
     }
 }
