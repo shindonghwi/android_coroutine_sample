12d8e2aef884f39e242aeba9acd7faa10d519492
shindonghwi <ehdgnl8940@naver.com>
2022-05-01 00:15:21 +0900

    feat: get random 함수 추가

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -1,6 +1,7 @@
 package wolf.shin.studycoroutine
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +11,42 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.*
 import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StudyCoroutineTheme {
+                runBlocking {
+                    val elapsedTime = measureTimeMillis {
+                        val value1 = getRandom1()
+                        val value2 = getRandom2()
 
+                        Log.d(TAG, "$value1 + $value2 = ${value1 + value2}")
+                    }
+                    Log.d(TAG, "$elapsedTime")
+                }
             }
         }
     }
+
+    companion object{
+        val TAG = "CoroutineStudy"
+    }
+
 }
+
+suspend fun getRandom1(): Int{
+    delay(1000L)
+    return Random.nextInt(0, 500)
+}
+
+suspend fun getRandom2(): Int{
+    delay(1000L)
+    return Random.nextInt(0, 500)
+}
+
