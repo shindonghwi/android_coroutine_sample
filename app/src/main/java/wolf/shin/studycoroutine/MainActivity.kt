d128b4656028bde32472db99ced8df402d1dd519
shindonghwi <ehdgnl8940@naver.com>
2022-05-01 21:05:30 +0900

    Volatile 어노테이션 사용

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import kotlinx.coroutines.*
 import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
 import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 val TAG = "CoroutineStudy"
 
@@ -15,36 +16,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudyCoroutineTheme {
-
-                runBlocking {
-
-                    CoroutineScope(Dispatchers.IO).launch {
-                        supervisoredFunc()
-                    }.join()
-
-                }
-
+                main()
             }
         }
     }
 
-    suspend fun printRandom1() {
-        delay(100L)
-        println(Random.nextInt(0, 500))
+    suspend fun massiveRun(action: suspend () -> Unit){
+        val n = 100 // 시작할 코루틴의 갯수
+        val k = 1000 // 코루틴 내에서 반복할 횟수
+
+        val elapsed = measureTimeMillis {
+            coroutineScope {
+                repeat(n){
+                    launch {
+                        repeat(k) { action() }
+                    }
+                }
+            }
+        }
+        println("$elapsed ms동안 ${n * k}개의 액션을 수행했다.")
     }
 
-    suspend fun printRandom2() {
-        delay(500L)
-        throw ArithmeticException()
-    }
+    @Volatile // 가시성 문제를 해결 할 수 있어도, 동시에 읽고 수정해서 생기는 문제는 해결 못한다.
+    var counter = 0
 
-    suspend fun supervisoredFunc() = supervisorScope {
-        launch { printRandom1() }
-        launch(ceh) { printRandom2() }
-    }
-
-    val ceh = CoroutineExceptionHandler { coroutineContext, throwable ->
-        println("Something happend: $throwable")
+    fun main() = runBlocking {
+        withContext(Dispatchers.Default){
+            massiveRun {
+                counter++
+            }
+        }
+        println("Counter = $counter")
     }
 
 }
