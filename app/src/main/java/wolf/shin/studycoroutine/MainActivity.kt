425e2afa8377a5fa8b47a9c3b4a67af966e99a26
shindonghwi <ehdgnl8940@naver.com>
2022-05-17 16:15:25 +0900

    feat: flow emit 과 flowOf 사용

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.activity.compose.setContent
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
 import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
 import kotlin.random.Random
 
@@ -24,23 +24,22 @@ class MainActivity : ComponentActivity() {
     }
 
     // 플로우 빌더를 통해 값을 방출하고, 수집기를 통해 값을 출력한다
-    fun flowSomething(): Flow<Int> = flow{
-        repeat(10){
+    fun flowSomething(): Flow<Int> = flow {
+        repeat(10) {
             emit(Random.nextInt(0, 500))
             kotlinx.coroutines.delay(1000L)
         }
     }
 
     fun main() = runBlocking {
-
-        val result = withTimeoutOrNull(500L){
-            flowSomething().collect { value -> println(value) }
-            true
-        }?:false
-
-        if (!result){
-            println("취소")
-        }
+        flowOf(1, 2, 3, 4, 5).collect { value -> print(value) }
+        flow {
+            emit(1)
+            emit(2)
+            emit(3)
+            emit(4)
+            emit(5)
+        }.collect { print(it) }
     }
     // 콜드 스트림 : 요청이 있는 경우에 대해서 1:1 로 값을 전달하기 시작
     // 핫 스트림: 0개 이상의 상대를 향해 지속적으로 값을 전달
