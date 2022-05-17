768c2275ce23c3b704424e3b14a65619ce235ec2
shindonghwi <ehdgnl8940@naver.com>
2022-05-17 15:52:16 +0900

    feat: flow 빌더와 collect를 사용하여 값을 출력하기

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -3,19 +3,16 @@ package wolf.shin.studycoroutine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.actor
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.random.Random
-import kotlin.system.measureTimeMillis
 
 val TAG = "CoroutineStudy"
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalStdlibApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,48 +22,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    suspend fun massiveRun(action: suspend () -> Unit){
-        val n = 100 // 시작할 코루틴의 갯수
-        val k = 1000 // 코루틴 내에서 반복할 횟수
-
-        val elapsed = measureTimeMillis {
-            coroutineScope {
-                repeat(n){
-                    launch {
-                        repeat(k) { action() }
-                    }
-                }
-            }
-        }
-        println("$elapsed ms동안 ${n * k}개의 액션을 수행했다.")
-    }
-
-    fun CoroutineScope.counterActor() = actor<CounterMsg> {
-        var counter = 0
-        for (msg in channel){
-            when(msg){
-                is IncCounter -> counter++
-                is GetCounter -> msg.response.complete(counter)
-            }
+    // 플로우 빌더를 통해 값을 방출하고, 수집기를 통해 값을 출력한다
+    fun flowSomething(): Flow<Int> = flow{
+        repeat(10){
+            emit(Random.nextInt(0, 500))
+            kotlinx.coroutines.delay(10L)
         }
     }
 
     fun main() = runBlocking {
-        val counter = counterActor()
-        withContext(Dispatchers.Default){
-            massiveRun {
-                counter.send(IncCounter)
-            }
-        }
-
-        val response = CompletableDeferred<Int>()
-        counter.send(GetCounter(response))
-        print("Counter: ${response.await()}")
-        counter.close()
+        flowSomething().collect { value -> println(value) }
     }
-
-    sealed class CounterMsg
-    object IncCounter : CounterMsg()
-    class GetCounter(val response: CompletableDeferred<Int>): CounterMsg()
-
 }
