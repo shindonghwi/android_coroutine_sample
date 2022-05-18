e49f2f04059c29fa45a6c044f9747ef12f9ccbd0
shindonghwi <ehdgnl8940@naver.com>
2022-05-18 15:08:56 +0900

    feat: conflate 연산자를 사용하여 받지 못한 값을 누락시킨다.

--- app/src/main/java/wolf/shin/studycoroutine/flow/Flow2-4.kt
+++ app/src/main/java/wolf/shin/studycoroutine/flow/Flow2-4.kt
@@ -3,6 +3,7 @@ package wolf.shin.studycoroutine.flow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
@@ -17,6 +18,17 @@ fun flowBufferSimple() = flow{
 fun flowBuffer() = runBlocking {
     val time = measureTimeMillis {
         flowBufferSimple().buffer().collect { value -> // buffer연산자를 사용하면 보내는쪽에서 더 이상 기다리지 않게한다.
+            delay(300L)
+            println(value)
+        }
+    }
+
+    println("Collected in $time ms")
+}
+
+fun flowConflate() = runBlocking {
+    val time = measureTimeMillis {
+        flowBufferSimple().conflate().collect { value -> // conflate 사용하면 처리하지 못하는동안 받았던 값을 누락시킨다.
             delay(300L)
             println(value)
         }
