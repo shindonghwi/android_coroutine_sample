513d32ea53df2b050b983ad2c008fbb3dab616f8
shindonghwi <ehdgnl8940@naver.com>
2022-05-20 00:12:26 +0900

    feat: 이미지 검색 api repository 추가

---
+++ app/src/main/java/wolf/shin/studycoroutine/naver_search/data/NaverImageSearchRepository.kt
@@ -0,0 +1,52 @@
+package wolf.shin.studycoroutine.naver_search.data
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import wolf.shin.studycoroutine.naver_search.api.NaverImageSearchService
+import wolf.shin.studycoroutine.naver_search.model.Item
+
+class NaverImageSearchRepository {
+    private val service: NaverImageSearchService
+
+    init {
+        val logger = HttpLoggingInterceptor()
+        logger.level = HttpLoggingInterceptor.Level.BASIC
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+
+                val request = chain.request().newBuilder()
+                    .addHeader("X-Naver-Client-Id", "hEcROD614ZSQ8LyPGntL")
+                    .addHeader("X-Naver-Client-Secret", "kvgAdUU70D")
+                    .build()
+                chain.proceed(request)
+            }
+            .addInterceptor(logger)
+            .build()
+
+        service = Retrofit.Builder()
+            .baseUrl("https://openapi.naver.com")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NaverImageSearchService::class.java)
+    }
+
+    fun getImageSearch(query: String): Flow<PagingData<Item>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = NaverImageSearchDataSource.defaultDisplay,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                NaverImageSearchDataSource(query, service)
+            }
+        ).flow
+    }
+}
