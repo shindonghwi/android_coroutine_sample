7521c9c6529fa88be3673185a4e427445d9cda5f
shindonghwi <ehdgnl8940@naver.com>
2022-05-20 00:32:16 +0900

    feat: 프래그먼트 탭레이아웃 구성

--- app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
+++ app/src/main/java/wolf/shin/studycoroutine/MainActivity.kt
@@ -1,94 +1,32 @@
 package wolf.shin.studycoroutine
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import wolf.shin.studycoroutine.channel.FanInOut4
-import wolf.shin.studycoroutine.channel.channelBuffer1
-import wolf.shin.studycoroutine.channel.channelBuffer2
-import wolf.shin.studycoroutine.channel.channelBuffer3
-import wolf.shin.studycoroutine.ui.theme.StudyCoroutineTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import wolf.shin.studycoroutine.databinding.ActivityMainBinding
+import wolf.shin.studycoroutine.ui.SectionsPagerAdapter
 
 val TAG = "CoroutineStudy"
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            StudyCoroutineTheme {
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-                /** Flow 2-2 연산자 */
-//                main_flow_2_2()
-//                flowFilter()
-//                flowFilterNot()
-//                flowMapAndFilterNot()
-//                flowTransform()
-//                flowTake()
-//                flowTakeWhile()
-//                flowDrop()
-//                flowDropWhile()
-//                flowReduce()
-//                flowFold()
-//                flowCount()
-
-                /** Flow 2-3 플로우 컨텍스트 */
-//                main_flow_2_3()
-//                flowSimpleWithContext()
-//                flowSimpleFlowOn()
-
-                /** Flow buffer */
-//                flowBuffer()
-//                flowConflate()
-//                flowCollectLatest()
-
-                /** Flow 결합 */
-//                flowZip()
-//                flowCombine()
-
-                /** Flow 플래트닝 */
-//                flowFlatMapConcat()
-//                flowFlatMapMerge()
-//                flowFlatMapLatest()
-
-                /** Flow 예외처리 */
-//                flowTryCatch()
-//                flowTryCatch1()
-//                flowTryCatch2()
-//                flowTryCatch3()
-
-                /** Flow 완료처리 */
-//                flowFinally1()
-//                flowFinally2()
-
-                /** Flow 런칭 */
-//                flowEvent1()
-//                flowEvent2()
-
-                /** Channel 기초 */
-//                channel1()
-//                channel2()
-//                channel3()
-//                channelClose1()
-//                channelProducer1()
-
-                /** Channel 파이프 라인 */
-//                channelPipeLine1()
-//                channelPipeLine2()
-//                channelPipeLine3()
-
-                /** Channel 채널 팬아웃, 팬인 */
-//                FanInOut1()
-//                FanInOut2()
-//                FanInOut3()
-//                FanInOut4()
-
-                /** Channel 버퍼링 */
-//                channelBuffer1()
-//                channelBuffer2()
-                channelBuffer3()
-
-            }
-        }
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
     }
+
 }
