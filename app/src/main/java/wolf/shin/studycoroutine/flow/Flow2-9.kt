f91aa664e3f14fe9b2cb1dec0912df531ae61374
shindonghwi <ehdgnl8940@naver.com>
2022-05-19 16:05:28 +0900

    feat: onEach는 collect가 있어야 동작한다.

--- app/src/main/java/wolf/shin/studycoroutine/flow/Flow2-9.kt
+++ app/src/main/java/wolf/shin/studycoroutine/flow/Flow2-9.kt
@@ -9,6 +9,10 @@ import kotlinx.coroutines.runBlocking
 fun events() = (1..3).asFlow().onEach { delay(300) }
 
 fun flowEvent1() = runBlocking {
+
+    // onEach는 collect가 실행되어야 동작한다.
+    // 이런식으로 동작하게된다면 main에서 Ui의 멈춤이 발생하여 ANR이 발생할 가능성이 매우높다
+
     events()
         .onEach { event -> println("Event: $event") }
         .collect()
