e531d7e81cd3f3559d0a7366ce05d75849b514f3
shindonghwi <ehdgnl8940@naver.com>
2022-05-20 00:32:02 +0900

    feat: 이미지 검색 뷰모델 생성

---
+++ app/src/main/java/wolf/shin/studycoroutine/ui/ImageSearchViewModel.kt
@@ -0,0 +1,48 @@
+package wolf.shin.studycoroutine.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
+import wolf.shin.studycoroutine.naver_search.data.NaverImageSearchRepository
+import wolf.shin.studycoroutine.naver_search.model.Item
+
+class ImageSearchViewModel : ViewModel() {
+    private val repository = NaverImageSearchRepository()
+    private val queryFlow = MutableSharedFlow<String>()
+    private val favorites = mutableSetOf<Item>()
+    private val _favoritesFlow = MutableSharedFlow<List<Item>>()
+
+    val pagingDataFlow = queryFlow
+        .flatMapLatest {
+            searchImages(it)
+        }
+        .cachedIn(viewModelScope)
+
+    val favoritesFlow = _favoritesFlow.asSharedFlow()
+
+    private fun searchImages(query: String): Flow<PagingData<Item>> =
+        repository.getImageSearch(query)
+
+    fun handleQuery(query: String) {
+        viewModelScope.launch {
+            queryFlow.emit(query)
+        }
+    }
+
+    fun toggle(item: Item) {
+        if (favorites.contains(item)) {
+            favorites.remove(item)
+        } else {
+            favorites.add(item)
+        }
+        viewModelScope.launch {
+            _favoritesFlow.emit(favorites.toList())
+        }
+    }
+}
