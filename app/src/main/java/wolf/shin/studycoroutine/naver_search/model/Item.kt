a25d4cf21e775792dcddfc777f625383cd4d5864
shindonghwi <ehdgnl8940@naver.com>
2022-05-20 00:08:59 +0900

    feat: 이미지 검색 api 컨텐츠 data 모델 정의

---
+++ app/src/main/java/wolf/shin/studycoroutine/naver_search/model/Item.kt
@@ -0,0 +1,9 @@
+package wolf.shin.studycoroutine.naver_search.model
+
+data class Item(
+    val title: String,
+    val link: String,
+    val thumbnail: String,
+    val sizeHeight: Int,
+    val sizeWidth: Int
+)
